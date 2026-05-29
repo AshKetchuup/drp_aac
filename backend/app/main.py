ea0065189576ee949f7e9ec39a9245060418375c
Ashrith Agastyaraju <agastyarajuAshrith@gmail.com>
2026-05-29 01:49:18 +0100

    [apa24] feat: implement mock calls to the api

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,14 +1,23 @@
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.seed import seed_data
 from app.routers import students, minigames, assignments, predictions
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    seed_data()
+    # seed_data() # Commented out to avoid firebase crash when running mockup
     yield
 
 app = FastAPI(title="AAC Homework API", lifespan=lifespan)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(students.router)
 app.include_router(minigames.router)
