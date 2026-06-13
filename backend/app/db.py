d9075f12d5e30af486b46a6a9bb5e3e8a2572797
Potato Jackson <58367167+potato-jackson@users.noreply.github.com>
2026-06-13 18:44:39 +0100

    fix: need to give db a name

--- backend/app/db.py
+++ backend/app/db.py
@@ -15,7 +15,7 @@ from motor.motor_asyncio import AsyncIOMotorClient, AsyncIOMotorGridFSBucket
 MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017")
 
 client: AsyncIOMotorClient = AsyncIOMotorClient(MONGO_URI)
-db = client.get_default_database()
+db = client["aac"]
 
 # Collections
 profiles = db["profiles"]
