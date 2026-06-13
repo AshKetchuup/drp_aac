84a2e3bc5f12c18d4406736d821487dd6957c5de
Potato Jackson <58367167+potato-jackson@users.noreply.github.com>
2026-06-13 18:36:55 +0100

    fix: wrong syntax for function name

--- backend/app/db.py
+++ backend/app/db.py
@@ -15,7 +15,7 @@ from motor.motor_asyncio import AsyncIOMotorClient, AsyncIOMotorGridFSBucket
 MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017")
 
 client: AsyncIOMotorClient = AsyncIOMotorClient(MONGO_URI)
-db = client.getDefaultDatabase()
+db = client.get_default_database()
 
 # Collections
 profiles = db["profiles"]
